50103e4dfcbb9e728e8422f5c54e28de13a3b19d
f1studioz <vipin.gopinath@f1studioz.com>
2016-08-28 14:27:14 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 /* Add your custom JavaScript code */
 
 $(document).ready(function(){
+    if($('body').width()>800){
      $(window).scroll(function () { 
         if($(window).scrollTop() >150){
             $('#header').css({background:'#fff'});
@@ -8,5 +9,5 @@ $(document).ready(function(){
         else{
             $('#header').css({background:'rgba(255,255,255,0.7)'});
         }
-    });
+    });}
 });
